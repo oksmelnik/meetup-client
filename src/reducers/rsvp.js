8b1be0dfd4295a55218bc172aacb3d6498e6bdf5
Oksana Melnik <oksana@dedact.nl>
2019-08-29 08:29:41 +0200

    fix issue with duplicated items

--- src/reducers/rsvp.js
+++ src/reducers/rsvp.js
@@ -1,11 +1,15 @@
 import { ADD_RSVP } from '../actions/actions'
 import { rsvp } from '../lib/rsvp'
-
+//https://css-tricks.com/learning-react-redux/
 export default (state = rsvp, { type, payload } = {}) => {
   switch (type) {
     case ADD_RSVP :
-      return [payload, ...state]
 
+    if (state.filter(i => i.rsvp_id === payload.rsvp_id).length === 0) {
+        return [...state, payload]
+    } else {
+        return state
+    }
 
       default :
       return state
